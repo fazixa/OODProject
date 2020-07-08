9fab6556e84ae12837467a1ce7c473092e04ee74
Faezeh <shayesteh.fa@gmail.com>
2020-07-08 15:16:21 +0430

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # OODProject
 Object Oriented Design Course Project
+###Journal publication system implementing GoF Structural Patterns
---
+++ src/com/finalProject/Main.java
@@ -0,0 +1,8 @@
+package com.finalProject;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
